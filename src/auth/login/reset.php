0f0cbd14a9da62277b5e948d196d6c7c5e50706e
kazuki663 <kazkaz.t@icloud.com>
2022-09-07 17:12:33 +0900

    パスワード再設定完了

--- src/auth/login/reset.php
+++ src/auth/login/reset.php
@@ -2,33 +2,38 @@
     session_start();
     require('../../dbconnect.php');
     $error = [];
-    // echo $_SESSION['reset_mail'];
+    $newPassword= $_POST['new'];
     $reset_mail = $_SESSION['reset_mail'];
-    $new_password = sha1($_POST['new']);
-    $stmt = $db->prepare('SELECT count(*) from users where password=?');
-    $stmt->bindValue(1, $new_password, PDO::PARAM_STR);
-    $stmt->execute();
-    $exist = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($_POST){
+    if ($newPassword === '') {
+        $err_msg['password'] = '入力してください';
+    }
+      // 文字数チェック
+    elseif (strlen($newPassword) > 255 || strlen($newPassword) < 5) {
+        $err_msg['password'] = '６文字以上２５５文字以内で入力してください';
+    }
+      // 形式チェック
+    elseif (!preg_match("/^[a-zA-Z0-9]+$/", $newPassword)) {
+        $err_msg['password'] = '半角英数字で入力してください';
+    }
+    }
 
-    if (!empty($_POST)) {
-    if (intval($exist['count(*)'] == 0)) {
-    if (sha1($_POST['new']) == sha1($_POST['new_check'])){
-    $stmt = $db->prepare('UPDATE `users` SET password=? WHERE `email`=?');
-    $stmt->bindValue(1, $new_password, PDO::PARAM_STR);
-    $stmt->bindValue(2, $reset_mail, PDO::PARAM_STR);
-    $stmt->execute();
-    $error['change'] = 'nothing';
-    // header('Location: http://' . $_SERVER['HTTP_HOST'] . '/client/login.php');
-    // exit();
-    }else{
-        $error['change'] = 'no_match';
-        echo '確認用と一致しませんでした';
+    if(empty($err_msg)){
+        if (!empty($_POST)) {
+        if ($_POST['new'] == $_POST['new_check']){
+        $new_password = password_hash($_POST['new'], PASSWORD_DEFAULT);
+        $stmt = $db->prepare('UPDATE `users` SET password=? WHERE `email`=?');
+        $stmt->bindValue(1, $new_password, PDO::PARAM_STR);
+        $stmt->bindValue(2, $reset_mail, PDO::PARAM_STR);
+        $stmt->execute();
+        $error['change'] = 'nothing';
+        // header('Location: http://' . $_SERVER['HTTP_HOST'] . '/client/login.php');
+        }else{
+            $error['change'] = 'no_match';
+            echo '確認用と一致しませんでした';
+        }
     }
-    }else{
-        $error['change'] = 'no_one';
-        echo '同じパスワードがすでに使われております。別のパスワードにしてください';
     }
-};
 ?>
 
 <!DOCTYPE html>
@@ -54,9 +59,11 @@
             <?php if (isset($error['change']) && $error['change'] === 'no_match') : ?>
                 <span>確認用と一致しませんでした</span>
             <?php endif; ?>
-            <?php if (isset($error['change']) && $error['change'] === 'no_one') : ?>
-                <span>同じパスワードがすでに使われております。別のパスワードにしてください</span>
-            <?php endif; ?>
+            <?php
+            if (!empty($err_msg['password'])){
+                echo $err_msg['password'];
+            }
+            ?>
             <p><input type="submit" value="確定"></p>
             <?php if (isset($error['change']) && $error['change'] === 'nothing') : ?>
                 <span>パスワードが変更されました、再ログインしてください</span>
