8d482f62ad67ae4a9923f05fee7199843c072c6d
Naoki Nishiyama <naoki1010nissy@gmail.com>
2022-09-07 16:56:09 +0900

    あとは任せた

--- src/header.php
+++ src/header.php
@@ -13,14 +13,12 @@
 
 <nav class="nav">
   <ul class="font-bold">
-    <li><a href="#">イベント一覧</a></li>
-    <li><a href="#">CSVダウンロード</a></li>
+    <li><a href="adminTop.php">イベント一覧</a></li>
     <li><a href="addEvent.php">イベント追加</a></li>
-    <li class="last-sentense"><a href="addMember.php">ユーザー追加・募集</a></li>
+    <li class="last-sentense"><a href="addMember.php">ユーザー追加</a></li>
   </ul>
   <div class="touser">
-    <a href="adminLogin.php">管理者画面へ</a><br><br><br>
-    <a href="adminTop.php" class="mt-10">トップページへ</a>
+    <a href="../auth/login/index.php">ユーザ画面へ</a><br><br><br>
   </div>
 </nav>
   </header>
