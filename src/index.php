1fe9ec12bbe5024f628da99e8f86ec2a6f2745f6
田上黎 <tanouerei@tanouereinoMacBook-Air.local>
2022-09-07 14:07:37 +0900

    不参加と未提出の部分のコメントアウト

--- src/index.php
+++ src/index.php
@@ -45,8 +45,8 @@ function get_day_of_week ($w) {
         <div class="flex">
           <a href="" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-blue-600 text-white">全て</a>
           <a href="" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">参加</a>
-          <a href="" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">不参加</a>
-          <a href="" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">未回答</a>
+          <!-- <a href="" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">不参加</a> -->
+          <!-- <a href="" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">未回答</a> -->
         </div>
       </div>
 
