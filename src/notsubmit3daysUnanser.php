cf699d3b563333b5b45d5896b2db0ee04b6aaa30
田上黎 <tanouerei@tanouereinoMacBook-Air.local>
2022-09-08 17:31:06 +0900

    前日通知を3日前通知に変更

--- src/notsubmit3daysUnanser.php
+++ src/notsubmit3daysUnanser.php
@@ -5,7 +5,7 @@ mb_language('ja');
 mb_internal_encoding('UTF-8');
 
 
-$objDateTime = new DateTime('tomorrow');
+$objDateTime = new DateTime('+3 day ');
 $event_day=$objDateTime->format('Y-m-d');
 $stmt = $db->query("SELECT events.id AS eventId, event_attendance.id, events.name, events.start_at,events.end_at,events.detail,event_attendance.user_id,users.name AS userName,users.email AS userEmail,event_attendance.participation,event_attendance.nonparticipation,event_attendance.notsubmitted, count(event_attendance.id) AS total_participants  
                     FROM event_attendance 
@@ -17,7 +17,7 @@ $stmt = $db->query("SELECT events.id AS eventId, event_attendance.id, events.nam
                     ASC");
 $stmt->execute();
 $events = $stmt->fetchAll();
-$title = "イベント前日です。このメールが送られた方は今すぐ回答をお願いします。";
+$title = "イベント3日前です。このメールが送られた方は今すぐ回答をお願いします。";
 echo ($event_day);
 
 require('notsubmittedmail.php');
