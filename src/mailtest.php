007f9008f1b1d2ec2378ed2d7d0b40cb1e94ab08
田上黎 <tanouerei@tanouereinoMacBook-Air.local>
2022-09-08 12:12:46 +0900

    メールテスト　いったん

--- src/mailtest.php
+++ src/mailtest.php
@@ -8,99 +8,40 @@ mb_language('ja');
 mb_internal_encoding('UTF-8');
 
 
-$event_day = date("Y-m-d", strtotime('+3 day')). "\n";
+$event_day = date("Y-m-d", strtotime('+1 day')). "\n";
 $stmt = $db->query("SELECT events.id AS eventId, event_attendance.id, events.name, events.start_at,events.end_at,event_attendance.user_id,users.name AS userName,users.email AS userEmail,event_attendance.participation,event_attendance.nonparticipation,event_attendance.notsubmitted, count(event_attendance.id) AS total_participants  
                     FROM event_attendance 
                     INNER JOIN events ON event_attendance.event_id=events.id 
                     INNER JOIN users ON event_attendance.user_id=users.id 
-                    WHERE  DATE_FORMAT(events.start_at, '%Y-%m-%d') >= '$event_day' 
+                    WHERE  DATE_FORMAT(events.start_at, '%Y-%m-%d') = '$event_day' 
                     GROUP BY  event_attendance.id 
                     ORDER BY events.start_at 
                     ASC");
 $stmt->execute();
 $events = $stmt->fetchAll();
 
-$to = "hackathon-teamX@posse-ap.com";
-$subject = "PHPからメール送信サンプル";
-$body = "本文";
-$headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
+foreach ( $events as $event){
 
-$name = "テスト";
-$date = "2021年08月01日（日） 21:00~23:00";
-$event = "縦モク";
-$body = 
-<<<EOT
-{$name}さん
-${date}に${event}を開催します。
-参加／不参加の回答をお願いします。
-http://localhost/
-EOT;
-
-mb_send_mail($to, $subject, $body, $headers);
-echo "donedone";
-echo "$to";
-echo "$subject";
-echo "$body";
-echo "$headers";
-
-// if (mail("example@example.com", "TEST MAIL", "This is a test message.", "From: from@example.com")) {
-//     echo "メールが送信されました。";
-//   } else {
-//     echo "メールの送信に失敗しました。";
-//   }
-  
-
-// $event_day = date("Y-m-d", strtotime('+3 day')). "\n";
-// $stmt = $db->query("SELECT events.id 
-//                     AS eventId, event_attendance.id, events.name, events.start_at,events.end_at,event_attendance.user_id,event_attendance.participation,event_attendance.nonparticipation,event_attendance.notsubmitted, count(event_attendance.id) AS total_participants  
-//                     FROM event_attendance 
-//                     INNER JOIN events ON event_attendance.event_id=events.id 
-//                     WHERE  DATE_FORMAT(events.start_at, '%Y-%m-%d') = '$event_day' 
-//                     GROUP BY  event_attendance.id 
-//                     ORDER BY events.start_at 
-//                     ASC");
-// $stmt->execute();
-// $events = $stmt->fetchAll();
-
-// foreach ($events as $event){
-
-
-//     $event_name= $event['name'];
-//     $event_start_at= $event['start_at'];
-//     $event_end_at= $event['end_at'];
-
-//     $to = "";
-//     $subject = "イベント開催のお知らせ";
-//     $body = "$event";
-//     $headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
     
-//     $name = "博士太朗";
-//     $date = "$event_start_at ~ $event_end_at";
-//     $event = "$event_name";
-//     $body = <<<EOT
-//     {$name}さん
     
-//     ${date}に${event}を開催します。
-//     参加／不参加の回答をお願いします。
+    $to = $event['userEmail'];
+    $subject = "PHPからメール送信サンプル";
+    $headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
+    $user_name = $event['userName'];
+    $event_start_date = $event['start_at'];
+    $event_end_date = $event['end_at'];
+    $event_name = $event['name'];
+    $event_body =
+    <<<EOT
+    {$user_name}さん
+    ${event_start_date}~${event_end_date}に${event_name}を開催します。
+    参加／不参加の回答をお願いします。
+    http://localhost/
+    EOT;
     
-//     http://localhost/
-//     EOT;
-
-
-
-
-// if (mb_send_mail("example@example.com", $subject,$body , $headers)) {
-//     echo "メールが送信されました。";
-//   } else {
-//     echo "メールの送信に失敗しました。";
-//   }
-  
-  
-    
-    // mb_send_mail($to, $subject, $body, $headers);
-    // echo "メールを送信しました";
-    
-    // $condition = 'participation';
-    // var_dump($date);
-    // var_dump(getUserByAttendanceStatus($db, $condition));
-// }
+    mb_send_mail($to, $subject, $event_body, $headers);
+    // echo "$to";
+    // echo "$subject";
+    // echo "$event_body";
+    // echo "$headers";
+}
