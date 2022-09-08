b732a708c9c3ba4b6b132f3e8c9b082ea49fcdf2
田上黎 <tanouerei@tanouereinoMacBook-Air.local>
2022-09-08 13:40:08 +0900

    イベントの内容をメール内に記載

--- src/mailtest.php
+++ src/mailtest.php
@@ -10,7 +10,7 @@ mb_internal_encoding('UTF-8');
 
 $objDateTime = new DateTime('tomorrow');
 $event_day=$objDateTime->format('Y-m-d');
-$stmt = $db->query("SELECT events.id AS eventId, event_attendance.id, events.name, events.start_at,events.end_at,event_attendance.user_id,users.name AS userName,users.email AS userEmail,event_attendance.participation,event_attendance.nonparticipation,event_attendance.notsubmitted, count(event_attendance.id) AS total_participants  
+$stmt = $db->query("SELECT events.id AS eventId, event_attendance.id, events.name, events.start_at,events.end_at,events.detail,event_attendance.user_id,users.name AS userName,users.email AS userEmail,event_attendance.participation,event_attendance.nonparticipation,event_attendance.notsubmitted, count(event_attendance.id) AS total_participants  
                     FROM event_attendance 
                     INNER JOIN events ON event_attendance.event_id=events.id 
                     INNER JOIN users ON event_attendance.user_id=users.id 
@@ -33,12 +33,15 @@ foreach ( $events as $event){
     $event_start_date = $event['start_at'];
     $event_end_date = $event['end_at'];
     $event_name = $event['name'];
+    $event_detail = $event['detail'];
     $event_body =
     <<<EOT
     {$user_name}さん
     ${event_start_date}~${event_end_date}に${event_name}を開催します。
     参加／不参加の回答をお願いします。
+    イベント内容：${event_detail}
     http://localhost/
+
     EOT;
     
     mb_send_mail($to, $subject, $event_body, $headers);
